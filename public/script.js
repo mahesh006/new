e4eb3bddead9eb6b92eeb379583f7c070d777505
Mahesh <55144460+mahesh006@users.noreply.github.com>
2023-05-19 01:52:29 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io("/");
+const socket = io("https://news-6we9.onrender.com");
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
   path: "/peerjs",
